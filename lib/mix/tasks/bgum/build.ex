69fef204e7256b3193637cffd5c4ab8f8bbcc8a3
ian asaff <ian.asaff@gmail.com>
2015-08-26 07:34:23 -0400

    wip

--- lib/mix/tasks/bgum/build.ex
+++ lib/mix/tasks/bgum/build.ex
@@ -12,16 +12,21 @@ defmodule Mix.Tasks.Bgum.Build do
 
   `mix bgum.build`
   """
-
-  def run(args) do
-    Mix.Task.run("app.start", args)
+  def run([]) do
+    Mix.Task.run("app.start", [])
     #path = Path.expand(hd(args))
     run_in_dev_mode
   end
 
-  def run_in_dev_mode do
+  def run(args) do
+    Mix.Task.run("app.start", args)
+    #path = Path.expand(hd(args))
+    run_in_dev_mode(hd(args))
+  end
+
+  def run_in_dev_mode(path \\ "omg") do
     IO.puts "=====running in #{Mix.env} mode====="
-    Builder.build!("test/fixtures/omg")
+    Builder.build!("test/fixtures/#{path}")
     #Bgum.Config.before_build
     #content = get_content("#{File.cwd!}/content")
     #content |> IO.inspect
